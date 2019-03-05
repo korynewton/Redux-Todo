9157bda407d1d00170187deede7875fe619cbd40
korynewton <korynewtondev@gmail.com>
2019-03-05 12:04:22 -0800

    input field changehandler working

--- todo/src/containers/InputBar.js
+++ todo/src/containers/InputBar.js
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const InputBar = () => {
-  return (
-    <>
-      <input type="text"/>
-      <button>Add To Do item</button>
-    </>
-  )
+class InputBar extends React.Component {
+    state = {
+        inputField : ''
+    }
+
+    changeHandler = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <input name='inputField' onChange={this.changeHandler} value={this.state.inputField} type="text"/>
+                <button>Add To Do item</button>
+            </div>
+        )
+    }
+    
 }
 
 export default InputBar
